034eccd4738b05c968a8094b70c32974e438138a
qa-omqgq-1-eamv@example.com <qa-omqgq-1-eamv@example.com>
2023-02-15 08:03:25 +0000

    First commit of promoted project

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://x-15271.stoplight-dev.com/api/v1/projects/cHJqOjUyMw/spectral.js?branch_slug=main&token=5ac5ee45-c2c5-4e31-b079-1fb788140878";
+export default { extends: ruleset };
